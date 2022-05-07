98d9c84cf402c382e4c5cf68163cda2fac9ea7c9
Pronesti <dieh.diego@gmail.com>
2022-05-07 09:11:17 -0300

    Marvel API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="getComic">test</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import md5 from "md5";
+import axios from "axios";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      characters: [],
+      reversed: [],
+    };
+  },
+  methods: {
+    getComic() {
+      let ts = Math.floor(Date.now() / 1000);
+      let hash = md5(
+        ts +
+          process.env.VUE_APP_PRIVATE_API_KEY +
+          process.env.VUE_APP_PUBLIC_API_KEY
+      );
+      axios
+        .get("https://gateway.marvel.com/v1/public/characters", {
+          params: {
+            ts,
+            apikey: process.env.VUE_APP_PUBLIC_API_KEY,
+            hash,
+          },
+        })
+        .then((response) => {
+          this.reversed = response.data.data.results;
+          this.characters = this.reversed.reverse();
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
+};
 </script>
